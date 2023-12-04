85938fa2da898a275dfd0b9c6183915d1b448896
Andry Pebrianto <andrypeb227@gmail.com>
2023-12-04 22:04:18 +0700

    Initial Commit

---
+++ dockerfile/script.sh
@@ -0,0 +1,3 @@
+docker build -t andrypebrianto/namaimage folderdockerfile
+docker build -t andrypebrianto/namaimage folderdockerfile --progress=plain --no-cache
+docker build -t andrypebrianto/namaimage folderdockerfile --build-arg app=isiarg
